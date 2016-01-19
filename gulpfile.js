aec9551c0169bd72bcdeb206e7144e264a7ef653
Ryen Nelsen <ryennelsen@gmail.com>
2016-01-18 23:13:22 -0800

    Add build script

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var stylus = require('gulp-stylus');
+var rename = require('gulp-rename');
+
+var mainStyl = './styl/main.styl';
+
+gulp.task('build:dev', function() {
+    gulp.src(mainStyl)
+        .pipe(sourcemaps.init())
+        .pipe(stylus())
+        .pipe(sourcemaps.write())
+        .pipe(rename('baremetal.css'))
+        .pipe(gulp.dest('./build/development'));
+});
+
+gulp.task('build:prod', function() {
+    gulp.src(mainStyl)
+        .pipe(stylus({ compress: true }))
+        .pipe(rename('baremetal.min.css'))
+        .pipe(gulp.dest('./build/production'));
+    gulp.src(mainStyl)
+        .pipe(stylus())
+        .pipe(rename('baremetal.css'))
+        .pipe(gulp.dest('./build/production'));
+});
+
+gulp.task('stylus:watch', function() {
+    gulp.watch('./styl/**/*.styl', ['stylus:main']);
+});
+
+gulp.task('watch', ['build:dev', 'stylus:watch']);
+
+gulp.task('default', ['build:dev', 'build:prod']);
