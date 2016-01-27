a5ee26626690dc4a38100a134a92727174f342ef
Ryen Nelsen <ryennelsen@gmail.com>
2016-01-26 20:52:55 -0800

    Add nib support for CSS3 mixins

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@ var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var stylus = require('gulp-stylus');
 var rename = require('gulp-rename');
+var nib = require('nib');
 
 var mainStyl = './styl/main.styl';
 
 gulp.task('build:dev', function() {
     gulp.src(mainStyl)
         .pipe(sourcemaps.init())
-        .pipe(stylus())
+        .pipe(stylus({ use: nib() }))
         .pipe(sourcemaps.write())
         .pipe(rename('baremetal.css'))
         .pipe(gulp.dest('./build/development'));
@@ -16,11 +17,11 @@ gulp.task('build:dev', function() {
 
 gulp.task('build:prod', function() {
     gulp.src(mainStyl)
-        .pipe(stylus({ compress: true }))
+        .pipe(stylus({ compress: true, use: nib() }))
         .pipe(rename('baremetal.min.css'))
         .pipe(gulp.dest('./build/production'));
     gulp.src(mainStyl)
-        .pipe(stylus())
+        .pipe(stylus({ use: nib() }))
         .pipe(rename('baremetal.css'))
         .pipe(gulp.dest('./build/production'));
 });
